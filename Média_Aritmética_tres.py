4b7a4f3592bcf8084baee1e8b09d67a6ad673df3
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-14 19:08:54 -0300

    Create Média_Aritmética_tres.py
    media aritmetica de tres numeros

---
+++ Média_Aritmética_tres.py
@@ -0,0 +1,5 @@
+numero= float(input("Digite o primeiro numero = "))
+numero_dois = float(input("Digite o segundo numero = "))
+numero_tres = float(input("Digite o terceiro numero = "))
+divisao = numero + numero_dois+numero_tres/3
+print ("MÉDIA ARITMÉTICA = ",divisao)
