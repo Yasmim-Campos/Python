5b29bebfddf649358fc444a6fe24c32f414eed58
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-14 18:50:48 -0300

    Create Duas_idades.py
    Escreva um pograma q le duas idades e faça a soma, subtração, divisao, multiplicação entre elas

---
+++ Duas_idades.py
@@ -0,0 +1,7 @@
+numero= int(input("Digite o primeiro numero = "))
+numero_dois = int(input("digite o segundo numero = "))
+soma = numero + numero_dois
+subtracao = numero - numero_dois
+multiplicacao = numero * numero_dois
+divisao = numero/numero_dois
+print ("soma =",soma, "subtração =",subtracao,"multiplicação =",multiplicacao,"divisão =",divisao)
