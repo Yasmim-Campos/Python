8375b12172f181af6ec06187e1ef8c9b250cb32e
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-14 19:03:06 -0300

    Create Média_Aritmética.py
    pograma de média aritmética de dois números

---
+++ Média_Aritmética.py
@@ -0,0 +1,4 @@
+numero= float(input("Digite o primeiro numero = "))
+numero_dois = float(input("digite o segundo numero = "))
+divisao = numero + numero_dois/2
+print ("MÉDIA ARITMÉTICA = ",divisao)
