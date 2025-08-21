d8ebd41fa3180d21312c830589584f4b774bec9f
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-21 19:38:43 -0300

    Create Calculadora.py
    calculadora elif

---
+++ Calculadora.py
@@ -0,0 +1,19 @@
+numero= int(input("Digite o primeiro numero = "))
+numero_dois = int(input("digite o segundo numero = "))
+
+soma = numero + numero_dois
+subtracao = numero - numero_dois
+divisao = numero / numero_dois
+multiplicacao = numero * numero_dois
+
+operacao = int(input("soma = 1 ,subraçao = 2 ,divisão = 3 ,multiplicação = 4 :"))
+if operacao == 1 :
+    print(soma)
+elif operacao==2:
+     print = (subtracao)
+elif operacao ==3:
+     print = (divisao)            
+elif operacao==4:
+    print =( multiplicao )
+else :
+    print (" n valido")
