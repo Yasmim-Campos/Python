82dbf448776d3d79a789ae4ffd9e276d9a2ea1bc
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-21 18:40:43 -0300

    Create Idade_dirigir_02.py
    com else e if

---
+++ Idade_dirigir_02.py
@@ -0,0 +1,11 @@
+#Faça 1 pograma que solicita a idade de um pesssoa e informe
+#se pode dirigir ou nao
+
+idade = int(input("digite a idade : "))
+if idade<=0:
+    print("digite um valor maior ")
+else:
+    if :idade >= 18:
+     print("vc pode dirigir")
+    else :
+     print("nao pode dirigir ")
