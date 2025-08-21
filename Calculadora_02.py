f3ed0b4acb1b29ed1ce5ecf597fc33d05add82db
Yasmim-Campos <yzamimsilvacampos@gmail.com>
2025-08-21 19:40:11 -0300

    Create Calculadora_02.py
    calculadora com else

---
+++ Calculadora_02.py
@@ -0,0 +1,22 @@
+numero= int(input("Digite o primeiro numero = "))
+numero_dois = int(input("digite o segundo numero = "))
+
+soma = numero + numero_dois
+subtracao = numero - numero_dois
+divisao = numero / numero_dois
+multiplicacao = numero * numero_dois
+
+operacao = int(input("soma = 1 ,subraçao = 2 ,divisão = 3 ,multiplicação = 4 :"))
+if operacao == 1 :
+    print(soma)
+else:
+   if operacao==2:
+         print = (subtracao)
+   else:
+        if operacao ==3:
+             print = (divisao)            
+        else:
+            if operacao==4:
+                 print =( multiplicao )
+            else :
+                print (" n valido")
